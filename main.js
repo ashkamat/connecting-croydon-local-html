557d13308b26864c905152f85738da984f9b44fe
ashkamat <121319039+ashkamat@users.noreply.github.com>
2025-09-30 16:59:34 +0100

    MVP 2

--- main.js
+++ main.js
@@ -31,18 +31,18 @@ const baseUrl = 'https://photo-sphere-viewer-data.netlify.app/assets/';
 const nodes = [
     {
         id: '1',
-        panorama: "images/R0010042.JPG",
+        panorama: "images/1.jpg",
         thumbnail: "images/R0010042.JPG",
         name: 'One',
         caption: "go back outside",
         links: [{ nodeId: '2' }],
         // markers: [pin1],
         gps: [0, 0, 0],
-        sphereCorrection: { pan: '180deg' },
+        sphereCorrection: { pan: '0deg' },
     },
     {
         id: '2',
-        panorama: "images/R0010041.JPG",
+        panorama: "images/2.jpg",
         thumbnail: "images/R0010041.JPG",
         name: 'Two',
         caption: "go to receception reception",
@@ -55,40 +55,40 @@ const nodes = [
 
         {
         id: '3',
-        panorama: "images/R0010040.JPG",
+        panorama: "images/3.jpg",
         thumbnail: "images/R0010041.JPG",
-        name: 'Two',
+        name: 'Three',
         caption: "louge endtrance",
         links: [{ nodeId: '2' }, { nodeId: '4' }],
 
         gps: [0,2, 0],
-        sphereCorrection: { pan: '10deg' },
+        sphereCorrection: { pan: '60deg' },
     },
 
 
      {
         id: '4',
-        panorama: "images/R0010039.JPG",
+        panorama: "images/4.jpg",
         thumbnail: "images/R0010041.JPG",
-        name: 'Two',
+        name: 'Four',
         caption: "louge endtrance",
         links: [{ nodeId: '3' },{ nodeId: '5' }],
 
         gps: [0,3, 0],
-        sphereCorrection: { pan: '160' },
+        sphereCorrection: { pan: '0' },
     },
 
 
      {
         id: '5',
-        panorama: "images/R0010036.JPG",
+        panorama: "images/5.jpg",
         thumbnail: "images/R0010041.JPG",
-        name: 'Two',
+        name: 'Five',
         caption: "louge endtrance",
         links: [{ nodeId: '4' }],
 
         gps: [0,4, 0],
-        sphereCorrection: { pan: '160' },
+        sphereCorrection: { pan: '0' },
     },
     
     
@@ -98,7 +98,7 @@ const nodes = [
 
 new Viewer({
     container: 'viewer',
-    loadingImg: "images/preloader.gif",
+    loadingImg: "",
     touchmoveTwoFingers: true,
     mousewheelCtrlKey: true,
     defaultYaw: '0deg',
@@ -106,48 +106,12 @@ new Viewer({
 
     plugins: [
         MarkersPlugin,
-        VirtualTourPlugin.withConfig({
+    
+            VirtualTourPlugin.withConfig({
             positionMode: 'gps',
             renderMode: '3d',
             nodes: nodes,
             startNodeId: '1',
         }),
     ],
-});
-
-
-
-
-
-const container1 = document.querySelector("#viewer2")
-
-
-const myViewer1 = new Viewer({
-
- container: container1,
- panorama: "images/R0010042.JPG",
- caption: "caption", 
- description: "this is a description",
- loadingImg: "images/tbxhlogo.avif",
-sphereCorrection:{ pan:"180deg", tilt:0, roll: 0 },
-
-navbar: [
-        'zoom',
-        'move',
-        "fullscreen"
-    ],
-
-
-
-
-
-
-})
-
-
-
-
-
-    myViewer1.addEventListener('click', ({ data }) => {
-    console.log(`${data.rightclick ? 'right ' : ''}clicked at yaw: ${data.yaw} pitch: ${data.pitch}`);
 });
